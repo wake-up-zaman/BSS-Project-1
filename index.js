067731e4139e6cec78809548f618e823ec78a0b3
wakeupzaman909 <hadiuzzaman909@outlook.com>
2023-07-28 21:10:51 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+// const toggleBtn=document.querySelector('.toggle_btn')
+// const toggleBtnIcon=document.querySelector('.toggle_btn i')
+// const dropDownMenu=document.querySelector('dropdown_menu')
+
+// toggleBtn.onClick=function(){
+//   dropDownMenu.classList.toggle('open');
+//   console.log("button Clicked");
+// }
+
+
+// <!-- <div class="timer">
+// <div class="counter">
+//   <div class="timer-segment">
+//     <span id="hours-3">24</span>
+//     <span>Hrs</span>
+//   </div>
+//   <span class="time-separator">:</span>
+//   <div class="timer-segment">
+//     <span id="minutes-3">00</span>
+//     <span>Min</span>
+//   </div>
+//   <span class="time-separator">:</span>
+//   <div class="timer-segment">
+//     <span id="seconds-3">00</span>
+//     <span>Sec</span>
+//   </div>
+// </div>
+// </div> -->
+
+  
